8b6b0ecd9bffd53ed00b7d73a0d6ecb7aad7621c
gz <geoffrey.zoroddu@gmail.com>
2024-04-15 22:50:21 +0200

    Added vercel rewrite for script id path variable.

--- api/scripts.ts
+++ api/scripts.ts
@@ -2,12 +2,17 @@ import type {VercelRequest, VercelResponse} from '@vercel/node'
 
 class ScriptController {
     createScript(req: VercelRequest, res: VercelResponse): VercelResponse {
-        return res.send('123456');
+        const content = req.body;
+        return res.json({
+            id: '123456',
+            content
+        });
     }
 
     getScript(req: VercelRequest, res: VercelResponse): VercelResponse {
+        const {scriptId} = req.query;
         return res.json({
-            id: '123456',
+            id: scriptId,
             content: 'test'
         });
     }
@@ -23,6 +28,6 @@ export default function handler(req: VercelRequest, res: VercelResponse) {
             return controller.getScript(req, res);
         default:
             return res.status(405)
-                .send('Method Not Allowed');
+                .send('405 - Method Not Allowed');
     }
 }
